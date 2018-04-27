cb5e2e084b0aeb3cbbdf3ef482f568ea2674dfa8
Yeesian Ng <ngyeesian@gmail.com>
2018-04-27 18:58:58 -0400

    add docstrings

--- src/GeoDataFrames.jl
+++ src/GeoDataFrames.jl
@@ -3,10 +3,29 @@ module GeoDataFrames
     import ArchGDAL; const AG = ArchGDAL
     import DataFrames, DataStreams #, SQLQuery
 
+    """
+    Constructs a `DataFrames.DataFrame` from `layer`.
+
+    Where each `field` is a column, and each `feature` is a row. The name of
+    the geometry column (if any) defaults to `geometry0`.
+    """
     function geodataframe(layer::AG.FeatureLayer)
         DataStreams.Data.stream!(AG.Source(layer), DataFrames.DataFrame)
     end
 
+    """
+    Returns the dataset in `filename` as a `DataFrames.DataFrame`.
+
+    For details, see http://www.gdal.org/ogr_sql.html.
+
+    ### Arguments
+    * `filename`: the dataset to run the query on
+    * (optional) `query`: the SQL SELECT query to be run on the dataset/layer
+
+    ### Keyword Arguments
+    * `layer::Int` (default=`0`): the layer in the dataset to retrieve
+    * `dialect::String` (default=`SQLite`): the SQL dialect for `query` (if any)
+    """
     function read(filename::AbstractString; layer::Int=0)
         AG.registerdrivers() do
             AG.read(filename) do dataset
